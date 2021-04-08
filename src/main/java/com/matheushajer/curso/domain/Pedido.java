89eb37d598e115723d98d511cb5a5401ba62e672
Matheus Hajer de Melo <matheushajer@gmail.com>
2021-04-07 21:29:36 -0300

    @JsonFormat incluido

--- src/main/java/com/matheushajer/curso/domain/Pedido.java
+++ src/main/java/com/matheushajer/curso/domain/Pedido.java
@@ -15,6 +15,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
@@ -24,6 +25,8 @@ public class Pedido implements Serializable {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
+	@JsonFormat(pattern = "dd/MM/YYYY hh:mm")
 	private Date instante;
 	
 	@JsonManagedReference
